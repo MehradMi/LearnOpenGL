a1032eea4a3ac513bbfbbaee1a85a1ceb73a5e49
MehradMi <165255856+MehradMi@users.noreply.github.com>
2025-12-27 16:43:08 +0330

    Added HelloWindow to LearnOpenGL

---
+++ HelloWindow/main.cpp
@@ -0,0 +1,52 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+constexpr int  WINDOW_WIDTH_PX = 800;
+constexpr int  WINDOW_HEIGHT_PX = 600;
+const char* WINDOW_TITLE {"HelloWindow"};
+
+void framebuffer_size_callback(GLFWwindow* window, int new_width, int new_height);
+
+int main() {
+    glfwInit();                                                      // 1. Initializing GLFW
+    // Telling GLFW which version of OpenGL we want
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                   // 2. Configuring GLFW using the glfwWindowHint
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                   // 3. Same as (2)
+                                                                     
+    // Telling GLFW we want to use the core-profile
+    // core-profile: smaller subset of OpenGL feature without backwards-compatible features we no longer need.
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);   // 4. Same as (2)
+    
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH_PX, WINDOW_HEIGHT_PX, WINDOW_TITLE, NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    // Telling GLFW to make the context of the window the main context of the "current thread"
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, WINDOW_HEIGHT_PX, WINDOW_HEIGHT_PX);
+    
+    // Telling GLFW which callback function to use on every window resize
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int new_width, int new_height) {
+    glViewport(0, 0, new_width, new_height);
+}
